8f294ab01e3d69351ecd9f656624b1ac4c1aa548
Emanuele DelBono <emanuele@codiceplastico.com>
2017-02-07 19:20:08 +0100

    pricing

--- app/index.js
+++ app/index.js
@@ -2,18 +2,30 @@ const THC = ['T', 'H', 'C']
 
 
 module.exports = {
-  make: function(order){
-    [beverageType, sugar] = order.split(':')
+  make: function (order) {
+    [beverageType, sugar, money] = order.split(':')
     const beverage = {}
     if (THC.indexOf(beverageType) == -1) {
       return null
     }
-    beverage.type = beverageType
 
-    if (sugar){
+      const value = this.getPrice(beverageType) - parseFloat(money)
+      if (isNaN(value) || value > 0) {
+        return {
+          message: `${value.toFixed(2)}$ missing`
+        }
+      }
+
+    beverage.type = beverageType
+    if (sugar) {
       beverage.sugar = parseInt(sugar)
       beverage.stick = beverage.sugar > 0
     }
     return beverage
+  },
+  getPrice: function (beverageType){
+    const priceList = { T : 0.4, H : 0.5, C : 0.6 }
+    return priceList[beverageType]
   }
 }
+
--- test/coffee_test.js
+++ test/coffee_test.js
@@ -7,55 +7,67 @@ describe('Coffe machine', function(){
   })
 
   it ('should make a tea',function(){
-    const tea = Coffee.make('T')
+    const tea = Coffee.make('T:0:0.4')
     assert.isNotNull(tea)
     assert.equal('T', tea.type)
   })
 
   it ('should make a coffee',function(){
-    const c = Coffee.make('C')
+    const c = Coffee.make('C:0:0.6')
     assert.isNotNull(c)
     assert.equal('C', c.type)
   })
 
   it ('should make a chocolate',function(){
-    const c = Coffee.make('H')
+    const c = Coffee.make('H:0:0.5')
     assert.isNotNull(c)
     assert.equal('H', c.type)
   })
 
   it ('beverage does not exists, should return null`',function(){
-    const c = Coffee.make('K')
+    const c = Coffee.make('K:0:0.4')
     assert.isNull(c)
   })
 
   it ('should make a coffee with 1 spoon of sugar',function(){
-    const c = Coffee.make('C:1')
+    const c = Coffee.make('C:1:0.6')
     assert.isNotNull(c)
     assert.equal('C', c.type)
     assert.equal(1, c.sugar)
   })
 
   it ('should make a tea with 2 spoons of sugar',function(){
-    const tea = Coffee.make('T:2')
+    const tea = Coffee.make('T:2:0.4')
     assert.isNotNull(tea)
     assert.equal('T', tea.type)
     assert.equal(2, tea.sugar)
   })
 
   it ('should make a tea with 2 spoons of sugar add stick',function(){
-    const tea = Coffee.make('T:2')
+    const tea = Coffee.make('T:2:0.4')
     assert.isNotNull(tea)
     assert.equal('T', tea.type)
     assert.equal(2, tea.sugar)
     assert.equal(true, tea.stick)
   })
 
-it ('should make a tea with 0 spoons of sugar without stick',function(){
-    const tea = Coffee.make('T:0')
+  it ('should make a tea with 0 spoons of sugar without stick',function(){
+    const tea = Coffee.make('T:0:0.4')
     assert.isNotNull(tea)
     assert.equal('T', tea.type)
     assert.equal(0, tea.sugar)
     assert.equal(false, tea.stick)
+  })
+
+  it ('should not make tea without enough money',function(){
+    const message = Coffee.make('T:0:0.3')
+    assert.isNotNull(message)
+    assert.equal('0.10$ missing', message.message)
+  })
+
+  it ('should not make coffee without enough money',function(){
+    const message = Coffee.make('C:0:0.5')
+    assert.isNotNull(message)
+    assert.equal('0.10$ missing', message.message)
   })
 })
