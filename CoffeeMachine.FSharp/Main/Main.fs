835db94a50045ba925eb26de84e3da8effff9f23
bauno <a.bonometti@gmail.com>
2017-02-23 12:55:22 +0100

    I have a great CoffeeMachine :-)

--- CoffeeMachine.FSharp/Main/Main.fs
+++ CoffeeMachine.FSharp/Main/Main.fs
@@ -4,13 +4,15 @@ open CoffeeMachine.Main
 [<EntryPoint>]
 let main argv =
     if Array.length argv <> 1
-    then printfn "Invalid Parameters: %A" argv
+    then printfn "Usage is CoffeeMachine report|<order>"
          -1
     else argv.[0]
-         |> make
          |> function
-         | Some beverage -> printfn "Here's your drink:\n\n %A\n" beverage
-         | None -> printfn "\nCouldn't make any drink. Look at the display"
+         | "report" -> printReceipt
+         | _  -> argv.[0]
+                |> make
+                |> function
+                | Some beverage -> printfn "Here's your drink:\n\n %A\n" beverage
+                | None -> printfn "\nCouldn't make any drink. Look at the display"
 
-         printReceipt
          0
