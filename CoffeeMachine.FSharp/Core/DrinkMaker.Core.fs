d79f35b0e5e69ca33f75063b3b7c6207c09955ab
bauno <a.bonometti@gmail.com>
2017-03-13 17:50:08 +0100

    Removed the ridicolous subtractfromsecond

--- CoffeeMachine.FSharp/Core/DrinkMaker.Core.fs
+++ CoffeeMachine.FSharp/Core/DrinkMaker.Core.fs
@@ -5,8 +5,11 @@ open DrinkMaker.OrderParser
 open QuantityChecker
 open System
 
-let private subtractFromSecond x y =
-  y - x
+let notEnoughMoney priceList order =  
+  (order.Beverage |> priceList) - order.MoneyInserted > 0.0
+
+let missingMoney priceList order =
+  (order.Beverage |> priceList) - order.MoneyInserted
 
 
 let makeBeverage''' parseOrder priceList beverageQuantityChecker orderStr =
@@ -16,8 +19,8 @@ let makeBeverage''' parseOrder priceList beverageQuantityChecker orderStr =
   match order.Beverage, order.MoneyInserted, order.ExtraHot, beverageQuantityChecker order.Beverage with
   | InvalidOrder, _, _, _ -> orderStr |>  sprintf "%s is not a valid order " |> Message
   | _, _, _, Some m -> m |> Message
-  | _, m, _, _ when ( order.Beverage |> priceList |> subtractFromSecond order.MoneyInserted) > 0.0 ->
-      order.Beverage |> priceList |> subtractFromSecond order.MoneyInserted
+  | _, m, _, _ when notEnoughMoney priceList order->
+      missingMoney priceList order
       |> sprintf "%.1f Euros missing"
       |> Message
   | b, _, h, _ when b = Orange && h -> "Cannot make an hot Orange Juice" |> Message
