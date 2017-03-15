af3121525c0d9c1c34fdcc785d74ef4f1f5d38dd
bauno <a.bonometti@gmail.com>
2017-03-15 12:55:34 +0100

    meh

--- CoffeeMachine.FSharp/Core/PriceList.fs
+++ CoffeeMachine.FSharp/Core/PriceList.fs
@@ -3,7 +3,7 @@ module CoffeeMachine.PriceList
 open System.Collections.Generic
 open DrinkMaker.Data
 
-let private prices = dict[Tea, 0.4; Coffee, 0.6; Chocolate, 0.5; Orange, 0.6]
+let private prices = [Tea, 0.4; Coffee, 0.6; Chocolate, 0.5; Orange, 0.6] |> dict
 
 let priceList beverageTYpe =
   prices.Item beverageTYpe
