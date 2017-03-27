72157ec1a0ebfc8142b363166be5ac2425624360
Alessandro Bonometti <bauno@bauniga.baita>
2017-03-27 12:26:22 +0200

    Tested OrderParser

--- CoffeeMachine.FSharp/Tests/OrderParser.Tests.fs
+++ CoffeeMachine.FSharp/Tests/OrderParser.Tests.fs
@@ -5,20 +5,55 @@ open Xunit
 open DrinkMaker.Core
 open CoffeeMachine.PriceList
 open DrinkMaker.Data
+open Microsoft.FSharp.Reflection
+open DrinkMaker.OrderParser.Main
+open Chessie.ErrorHandling
+
+let fromString<'a> (s:string) =
+    match FSharpType.GetUnionCases typeof<'a> |> Array.filter (fun case -> case.Name = s) with
+    |[|case|] -> Some(FSharpValue.MakeUnion(case,[||]) :?> 'a)
+    |_ -> None
+
+let extract = 
+    function 
+    | Some i ->i
+    | None -> failwith "Error!!"
+
+let extractOk =
+    function
+    | Ok(value, _) -> value
+    | Bad(_) -> failwith "Error!"
 
 
+[<Theory>]
+[<InlineData("C:2:0.9", "Coffee", 2, 0.9,false)>]
+[<InlineData("Ch:2:0.9", "Coffee", 2,0.9,true)>]
+[<InlineData("T:2:0.9", "Tea", 2, 0.9,false)>]
+[<InlineData("Th:2:0.9", "Tea", 2,0.9,true)>]
+[<InlineData("H:2:0.9", "Chocolate", 2, 0.9,false)>]
+[<InlineData("Hh:2:0.9", "Chocolate", 2,0.9,true)>]
+[<InlineData("O:2:0.9", "Orange", 2, 0.9,false)>]
 
-// [<Theory>]
-// [<InlineData("C:1:0.9", Chocolate, 0.6)>]
-// let ``I should make a drink if I have enough money`` (order: string) (bType: BeverageType) (sugar: int) (stick: bool) (price: float) =
-//  let getPrice beverageType =
-//    beverageType |> should equal bType
-//    price
 
-//  let drink = makeBeverage' getPrice order |> extract
+let ``I should be able to parse an order`` (order: string) (bType: string) (sugar: int) (moneyInserted: float) (extraHot: bool) =
 
-//  drink.Beverage |> should equal bType
-//  drink.Sugar |> should equal sugar
-//  drink.Stick |> should equal stick
+
+    let bev = 
+        {
+        Beverage = fromString<BeverageType> bType |> extract
+        ExtraHot = extraHot
+        MoneyInserted = moneyInserted
+        ListPrice = 0.0
+        Sugar = sugar
+        Stick = false
+    }
+
+    order 
+    |> parseOrder 
+    |> extractOk 
+    |> should equal bev
+
+    
+    
 
 
