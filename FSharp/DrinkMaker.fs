4961d1c920af2566c029e8adb81aa24d19d3d6d0
Alessandro Bonometti <bauno@bauniga.baita>
2017-03-28 10:52:19 +0200

    Deleted FSharp directory (obsolete)

--- FSharp/DrinkMaker.fs
+++
@@ -1,8 +0,0 @@
-module CoffeeMachine.Maker
-open DrinkMaker.Data
-open CoffeeMachine.PriceList
-open System.Text.RegularExpressions
-open System
-
-let makeBeverage orderStr =
-  DrinkMaker.Core.makeBeverage' priceList orderStr
--- FSharp/PriceList.fs
+++
@@ -1,9 +0,0 @@
-module CoffeeMachine.PriceList
-
-open System.Collections.Generic
-open DrinkMaker.Data
-
-let private prices = dict[Tea, 0.4; Coffee, 0.6; Chocolate, 0.5]
-
-let priceList beverageTYpe =
-  prices.Item beverageTYpe
