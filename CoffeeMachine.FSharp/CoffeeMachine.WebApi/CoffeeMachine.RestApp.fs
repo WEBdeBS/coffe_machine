ed821e5debfe76c6b72dbcf89a9f18016e737f06
Alessandro Bonometti <bauno@bauniga.baita>
2017-05-01 18:00:11 +0200

    restMachine!

---
+++ CoffeeMachine.FSharp/CoffeeMachine.WebApi/CoffeeMachine.RestApp.fs
@@ -0,0 +1,73 @@
+module CoffeeMachine.RestApp
+
+open Suave
+open Suave.Operators
+open Suave.Web
+open Suave.Http
+open Suave.Filters
+open Suave.Writers
+open Suave.RequestErrors
+open Suave.Successful
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+
+open CoffeeMachine.Main
+open DrinkMaker.Data
+
+
+let badRequest order =
+  order
+  |> sprintf "order %s not valid"
+  |> BAD_REQUEST
+
+
+let toJson v =
+  let jsonSerializerSettings = new JsonSerializerSettings()
+  jsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+  JsonConvert.SerializeObject(v, jsonSerializerSettings)
+
+let setCORSHeaders =
+    setHeader  "Access-Control-Allow-Origin" "*"
+    >=> setHeader "Access-Control-Allow-Headers" "content-type"
+
+let allowCors : WebPart =
+    choose [
+        OPTIONS >=>
+            fun context ->
+                context |> (
+                    setCORSHeaders
+                    >=> OK "CORS approved" )
+    ]
+
+let JSON v =
+  v
+  |> toJson
+  |> OK
+  >=> Writers.setMimeType "application/json; charset=utf-8"
+  >=> setCORSHeaders
+
+let handleResource requestError =
+    function
+    | Some r -> r |> JSON
+    | _ -> requestError
+
+
+let makeDrink order =
+  order
+  |> make
+  |> function
+  | Beverage beverage -> beverage |> JSON
+  | Message m -> badRequest m |> JSON
+
+let restMachine  =
+  choose
+    [
+      allowCors
+      GET >=> choose
+        [
+          pathScan "/order/%s" makeDrink
+          NOT_FOUND "Invalid route"
+        ]
+      POST >=> pathScan "/order/%s" (makeDrink)
+    ]
--- CoffeeMachine.FSharp/CoffeeMachine.WebApi/CoffeeMachine.WebApi.fs
+++ CoffeeMachine.FSharp/CoffeeMachine.WebApi/CoffeeMachine.WebApi.fs
@@ -3,9 +3,10 @@ module CoffeeMachine.WebApi
 open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
-open CoffeeMachine.Main
 open System
 open System.Net
+
+open CoffeeMachine.RestApp
 
 let ipZero = IPAddress.Parse("0.0.0.0")
 let port = System.Environment.GetEnvironmentVariable("PORT")
@@ -16,6 +17,6 @@ let cfg =
                  [ HttpBinding.create HTTP ipZero (uint16 port) ]
                }
 
-let drink = sprintf "%A" (make "Hh:1:0.9")
+//let drink = sprintf "%A" (make "Hh:1:0.9")
 
-startWebServer cfg (OK drink)
+startWebServer cfg restMachine
