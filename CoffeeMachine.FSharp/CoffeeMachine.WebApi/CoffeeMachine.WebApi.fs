293c39197657ebd45bc0350840ed8443afef9ef1
Alessandro Bonometti <bauno@bauniga.baita>
2017-05-01 16:25:27 +0200

    Moved WebApi

--- CoffeeMachine.FSharp/CoffeeMachine.WebApi/CoffeeMachine.WebApi.fs
+++ CoffeeMachine.FSharp/CoffeeMachine.WebApi/CoffeeMachine.WebApi.fs
@@ -1,6 +1,21 @@
 module CoffeeMachine.WebApi
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+open Suave                 // always open suave
+open Suave.Successful      // for OK-result
+open Suave.Web             // for config
+open CoffeeMachine.Main
+open System
+open System.Net
+
+let ipZero = IPAddress.Parse("0.0.0.0")
+let port = System.Environment.GetEnvironmentVariable("PORT")
+
+let cfg =
+        { defaultConfig with
+            bindings =
+                 [ HttpBinding.create HTTP ipZero (uint16 port) ]
+               }
+
+//printfn "%A" (make "Hh:1:0.9")
+
+startWebServer cfg (OK "La figa della Giacotti!!")
